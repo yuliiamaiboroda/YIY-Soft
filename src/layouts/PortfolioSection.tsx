96f83eeaa900de69c86e90f8b996f3df5e8c9baf
Iaroslav Seniuk <savuk7@icloud.com>
2023-11-13 00:07:23 +0200

    added portfolio section first markup

--- src/layouts/PortfolioSection.tsx
+++ src/layouts/PortfolioSection.tsx
@@ -1,5 +1,20 @@
+import PortfolioCard from '@/components/PortfolioCard';
+import Container from '@/layouts/Container';
+import Section from './Section';
+import SectionHeader from '../components/SectionHeader';
+
 interface IProps {}
 
 export default function PortfolioSection({}: IProps) {
-  return <div>PortfolioSection</div>;
+  return (
+    <Section>
+      <Container>
+        <SectionHeader>Portfolio</SectionHeader>
+        <div className="flex flex-col gap-10 md:gap-12 lg:gap-[60px]">
+          <PortfolioCard />
+          <PortfolioCard changeFlexDirection={true} />
+        </div>
+      </Container>
+    </Section>
+  );
 }
