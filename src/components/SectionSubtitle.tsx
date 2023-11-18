6482548b099a61a4131796a2518987207ed46b29
Iaroslav Seniuk <savuk7@icloud.com>
2023-11-19 00:54:48 +0200

    added feature section markup

--- src/components/SectionSubtitle.tsx
+++ src/components/SectionSubtitle.tsx
@@ -6,7 +6,7 @@ export default function SectionSubtitle({ children, ...rest }: IProps) {
   return (
     <h3
       {...rest}
-      className="mb-10 text-center text-24 font-medium text-primary md:mb-12  md:text-32 lg:mb-[60px] lg:text-36"
+      className="mx-auto mb-10 text-center text-24 font-medium text-primary md:mb-12  md:max-w-[520px] md:text-32 lg:mb-[60px] lg:max-w-[800px] lg:text-36"
     >
       {children}
     </h3>
