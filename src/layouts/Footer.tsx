0a4c5bc5cf5ec31b57142ca9c1d280284e240a9f
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-11-20 00:31:04 +0200

    Add footer and some fixes

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,5 +1,40 @@
+import Link from 'next/link';
+import Container from './Container';
+
 interface IProps {}
 
 export default function Footer({}: IProps) {
-  return <div className="mt-auto">Footer</div>;
+  return (
+    <footer className="bg-primary pb-5 pt-10 md:pb-6">
+      <Container>
+        <div
+          className="mb-6 flex flex-col items-center gap-6
+                    md:mb-9 md:flex-row md:justify-between"
+        >
+          <Link
+            href="/"
+            className="block h-[40px] w-[86px] md:h-[54px] md:w-[116px]"
+          >
+            <svg className="h-full w-full">
+              <use href="images/icons.svg#logo-lg"></use>
+            </svg>
+          </Link>
+          <div className="flex w-fit gap-1 text-16 text-secondary md:text-18">
+            <p>Designed by</p>
+            <a
+              href="https://www.linkedin.com/in/yelizavetatkachenko/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              Yelizaveta Tkachenko
+            </a>
+          </div>
+        </div>
+        <p className="mx-auto w-fit text-12 text-secondary md:text-16">
+          © 2023 All rights reserved.
+        </p>
+      </Container>
+    </footer>
+  );
 }
--- src/layouts/Section.tsx
+++ src/layouts/Section.tsx
@@ -1,10 +1,18 @@
 interface IProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
+  bgColor?: string;
 }
 
-export default function Section({ children, ...rest }: IProps) {
+export default function Section({
+  children,
+  bgColor = 'secondary',
+  ...rest
+}: IProps) {
   return (
-    <section {...rest} className="py-[50px] md:py-[60px] lg:py-[70px] xl:py-20">
+    <section
+      {...rest}
+      className={`bg-${bgColor} py-[50px] md:py-[60px] lg:py-[70px] xl:py-20`}
+    >
       {children}
     </section>
   );
