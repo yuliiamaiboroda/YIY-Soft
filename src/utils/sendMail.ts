8ed985eef490ae37bb9e3550dc3f93e5c04458a6
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-11-20 10:12:36 +0200

    Switch on form sender

--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -2,20 +2,17 @@ import type { IContactFormData } from '@/types';
 import { Notify } from 'notiflix';
 
 export const sendMail = async (data: IContactFormData) => {
-  // const apiEndpoint = 'api/email';
+  const apiEndpoint = 'api/email';
 
-  // fetch(apiEndpoint, {
-  //   method: 'POST',
-  //   body: JSON.stringify(data),
-  // })
-  //   .then(res => res.json())
-  //   .then(response => {
-  //     Notify.success(response.message);
-  //   })
-  //   .catch(err => {
-  //     Notify.failure(err);
-  //   });
-
-  console.log(data);
-  Notify.info('Turn on nodemailer!');
+  fetch(apiEndpoint, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  })
+    .then(res => res.json())
+    .then(response => {
+      Notify.success(response.message);
+    })
+    .catch(err => {
+      Notify.failure(err);
+    });
 };
